1b33f51bd51be920f4d2042db353a3b8c210efda
Hempratap Singh Solanki <hempratap2003@gmail.com>
2023-08-30 16:34:13 +0530

    web app

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
-from flask_bootstrap import Bootstrap5
-from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
-from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user, login_required
-from flask_sqlalchemy import SQLAlchemy
-from functools import wraps
-from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.orm import relationship
+from flask import Flask, render_template
 
-import os
-import smtplib
 
 app = Flask(__name__)
 
